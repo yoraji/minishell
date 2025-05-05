3c26823cf181e67692828622e199eef7035bd5c9
youssef raji <youssef@darkness.darkness>
2025-05-05 13:05:50 -0500

    i fix cd build in cmd

--- build-in/cd.c
+++ build-in/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoraji <yoraji@student.42.fr>              +#+  +:+       +#+        */
+/*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 08:50:53 by yoraji            #+#    #+#             */
-/*   Updated: 2025/04/29 14:22:47 by yoraji           ###   ########.fr       */
+/*   Updated: 2025/05/05 12:53:29 by youssef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,17 +48,18 @@ char *set_env_value(const char *name, const char *value) {
     return strdup(value);
 }
 
-int builtin_cd(ASTNode *node){
+int builtin_cd(ASTNode *node) {
     char *path;
     char cwd[1024];
     char *oldpwd;
 
+    printf("Executing cd command\n");
     if (!node || node->type != NODE_COMMAND || !node->args) {
         fprintf(stderr, "cd: missing argument\n");
         return -1;
     }
-    if (!node->args[1] || ft_strcmp(node->args[1], "~") == 0 ||
-        ft_strcmp(node->args[1], "$HOME") == 0 || ft_strcmp(node->args[1], "--") == 0) {
+
+    if (!node->args[1]) {
         path = get_env_value("HOME");
         if (!path) {
             fprintf(stderr, "cd: HOME not set\n");
@@ -70,11 +71,21 @@ int builtin_cd(ASTNode *node){
             fprintf(stderr, "cd: OLDPWD not set\n");
             return -1;
         }
-        printf("%s\n", path);
-    } else
+        printf("%s\n", path); // Print the new directory
+    } else if (ft_strcmp(node->args[1], "~") == 0 || ft_strcmp(node->args[1], "$HOME") == 0) {
+        path = get_env_value("HOME");
+        if (!path) {
+            fprintf(stderr, "cd: HOME not set\n");
+            return -1;
+        }
+    } else {
         path = node->args[1];
+    }
+
+    printf("Changing directory to: %s\n", path);
     if (!is_directory(path))
         return -1;
+
     oldpwd = get_env_value("PWD");
     if (chdir(path) != 0) {
         perror("cd");
@@ -89,6 +100,7 @@ int builtin_cd(ASTNode *node){
     if (oldpwd)
         set_env_value("OLDPWD", oldpwd);
     set_env_value("PWD", cwd);
+
+    printf("Directory changed successfully to: %s\n", cwd);
     return 0;
 }
-
--- main.AST.c
+++ main.AST.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 typedef  enum {
     NODE_COMMAND,
     NODE_PIPE,
